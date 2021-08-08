0e778949c41bc48cea351bf23d378494fc425017
0EFB6 <changws12345@gmail.com>
2021-08-08 20:57:25 +0800

    modified:   config.def.h

--- config.def.h
+++ config.def.h
@@ -1,6 +1,7 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+/* Change the value to your username */
+static const char *user  = "linux";
+static const char *group = "linux";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
